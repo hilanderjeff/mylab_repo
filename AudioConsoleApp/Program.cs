3e746579ae97dd8cb078e9c00fca48f8560fd755
Jeff Tung <jeff.tung@alcon.com>
2024-11-11 18:46:26 -0800

    origin

---
+++ AudioConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+using WinAudioMixerLib;
+
+class Program
+{
+    static void Main()
+    {
+        int processId = Process.GetProcessesByName("ConnectAgent").FirstOrDefault()?.Id ?? -1;
+
+        if (processId != -1)
+        {
+            VolumeMixer.SetApplicationVolume(processId, 0.5f);
+            // Set volume to 50%
+            float? currentVolume = VolumeMixer.GetApplicationVolume(processId);
+            Console.WriteLine($"Current Volume: {currentVolume}");
+        }
+        else
+        {
+            Console.WriteLine("Application not found.");
+        }
+    }
+}
+
